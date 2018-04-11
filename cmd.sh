801476cdccb700c197a39598e93e503fae861480
ph5il <ph5il@msn.com>
2018-04-11 14:13:25 -0500

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,1 @@
-bash <(/usr/bin/ruby -e "$(/usr/bin/curl -fsSL raw.githubusercontent.com/Homebrew/install/master/install)")
+/usr/bin/ruby -e "$(/usr/bin/curl -fsSL raw.githubusercontent.com/Homebrew/install/master/install)"
